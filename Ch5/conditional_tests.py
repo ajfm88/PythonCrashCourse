a7d61dbfe1d5a3a9de3eaa8477826b9274e1fe6e
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-01-30 22:01:10 -0800

    Just grinding

---
+++ Ch5/conditional_tests.py
@@ -0,0 +1,45 @@
+#5 different conditional tests
+
+#1
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+#2
+requested_topping = 'mushrooms'
+
+print("Is the requested topping == 'anchovies'? I predict False.")
+print(requested_topping == 'anchovies')
+
+print("Is the requested topping == 'mushrooms'? I predict True.")
+print(requested_topping == 'mushrooms')
+
+#3
+magical_number = '42'
+
+print("Is the magical number == '42'? I predict True.")
+print(magical_number == '42')
+
+print("Is the magical number == '76'? I predict False.")
+print(magical_number == '76')
+
+#4
+age = '21'
+
+print("Is the ideal age == '56'? I predict False.")
+print(age == '56')
+
+print("Is the ideal age == '21? I predict True.")
+print(age == '21')
+
+#5
+car = 'corvette'
+
+print("Is the best car == 'corvette'? I predict True.")
+print(car == 'corvette')
+
+print("Is the best car == 'camaro'? I predict False.")
+print(car == 'camaro')
