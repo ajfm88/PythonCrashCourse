0f93f2871d1c88ef94e56c22f727cc4bc8ead100
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-02-02 22:49:21 -0800

    Feb 2, 2021

---
+++ Ch6/glossary.py
@@ -0,0 +1,22 @@
+concepts = {
+    'variables': 'blah blah',
+    'asymptote': 'bleh bleh',
+    'derivative': 'blih blih',
+    'integrate': 'bloh bloh',
+    'corner': 'bluh bluh'
+}
+
+concept = 'variables'
+print(f"\n{concept.title()}: {concepts[concept]}")
+
+concept = 'asymptote'
+print(f"\n{concept.title()}: {concepts[concept]}")
+
+concept = 'derivative'
+print(f"\n{concept.title()}: {concepts[concept]}")
+
+concept = 'integrate'
+print(f"\n{concept.title()}: {concepts[concept]}")
+
+concept = 'corner'
+print(f"\n{concept.title()}: {concepts[concept]}")
