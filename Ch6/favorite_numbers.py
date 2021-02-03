19f687879db2378398e1aa9dd2c3c0113eaabe7f
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-02-02 22:32:56 -0800

    Feb 2, 2021

---
+++ Ch6/favorite_numbers.py
@@ -0,0 +1,26 @@
+#five names, use them as keys in your dictionary
+#five numbers for each person, store each as values in my dictionary.
+#print each person's name and their favorite number.
+
+favorite_numbers = {
+    'kojima': 34,
+    'miyamoto': 25,
+    'molyneux': 14,
+    'hamilton': 7,
+    'dostoevsky': 10
+}
+
+number = favorite_numbers['kojima']
+print(f"Kojima's favorite number is {number}")
+
+number = favorite_numbers['miyamoto']
+print(f"Miyamoto's favorite number is {number}")
+
+number = favorite_numbers['molyneux']
+print(f"Molyneux's favorite number is {number}")
+
+number = favorite_numbers['hamilton']
+print(f"Hamilton's favorite number is {number}")
+
+number = favorite_numbers['dostoevsky']
+print(f"Dostoevsky's favorite number is {number}")
---
+++ Ch6/person.py
@@ -0,0 +1,14 @@
+#Use a dictionary to store information about a person you know.
+#First name, last name, age, and the city in which they live.
+#keys first_name, last_name, age, and city.
+#Print each piece of information stored in your dictionary.
+
+person = {'first_name': 'Dwayne', 'last_name': 'Johnson', 'age': 48, 'city': 'Southwest Ranches'}
+
+print(f"This person's first name is {person['first_name']}!")
+
+print(f"This person's last name is {person['last_name']}!")
+
+print(f"This person's age is {person['age']}!")
+
+print(f"This person's city is {person['city']}!")
