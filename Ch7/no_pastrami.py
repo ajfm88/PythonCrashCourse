7234ab179cb6c3d5694ebabe52a8d48a6337524c
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-02-09 23:47:21 -0800

    Feb 9, 2021

--- Ch7/no_pastrami.py
+++ Ch7/no_pastrami.py
@@ -1,11 +1,18 @@
-sandwich_orders = ['tuna sandwich', 'pastrami sandwich', 'cuban sandwich']
+sandwich_orders = [
+    'pastrami', 'veggie', 'grilled cheese', 'pastrami',
+    'turkey', 'roast beef', 'pastrami']
 finished_sandwiches = []
 
+print("I'm sorry, we're all out of pastrami today.")
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+
+print("\n")
 while sandwich_orders:
     current_sandwich = sandwich_orders.pop()
-    print(f"I made you a {current_sandwich.title()}!")
+    print(f"I'm working on your {current_sandwich} sandwich.")
     finished_sandwiches.append(current_sandwich)
 
-print("\nThe following sandwiches have been made: ")
-for finished_sandwich in finished_sandwiches:
-    print(finished_sandwich.title())
+print("\n")
+for sandwich in finished_sandwiches:
+    print(f"I made a {sandwich} sandwich.")
