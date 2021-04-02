84501bb04b4a6b8d94dd86ff4f61267ebfe77a0d
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-04-02 00:16:20 -0700

    April 2, 2021

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -6,6 +6,9 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 #Alien Invasion starts as a class called AlienInvasion. 
 
 class AlienInvasion:
@@ -21,22 +24,26 @@ class AlienInvasion:
         #We import Settings into the main program file. Then we create an
         #instance of Settings and assign it to self.settings
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_Width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         #When we create a screen, we use the screen_width and
         #screen_height attributes of self.settings, 
-
         #we call pygame.display.set_mode() to create a display window, 
         # on which we’ll draw all the game’s graphical ele­ments.
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        #The argument (1200, 800) is a tuple that defines the dimensions of
+        #the game window, which will be 1200 pixels wide by 800 pixels high. (You
+        #can adjust these values depending on your display size.) We assign this dis­
+        #play window to the attribute self.screen, so it will be available in all methods
+        #in the class.
 
-#The argument (1200, 800) is a tuple that defines the dimensions of
-#the game window, which will be 1200 pixels wide by 800 pixels high. (You
-#can adjust these values depending on your display size.) We assign this dis­
-#play window to the attribute self.screen, so it will be available in all methods
-#in the class.
+        self.ship = Ship(self)
+        #We import Ship and then make an instance of Ship after the screen has been created
+        #The call to Ship() requires one argument, an instance of AlienInvasion. The self
+        #argument here refers to the current instance of AlienInvasion.
+        #This is the paramenter that gives Ship access to the game's resources,
+        #such as the screen object. We assign this Ship instance to self.ship
 
         #Set the background color.
         self.bg_color = (230, 230, 230)
@@ -52,34 +59,76 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse movements.
-            # This for loop is an event loop.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-#To access the events that Pygame detects, we’ll use the pygame.event.get() function.
-#This function returns a list of events that have taken place
-#since the last time this function was called. Any keyboard or mouse event
-#will cause this for loop to run. Inside the loop, we’ll write a series of if
-#statements to detect and respond to specific events. For example, when the
-#player clicks the game window’s close button, a pygame.QUIT event is detected
-#and we call sys.exit() to exit the game 
+            self._check_events()
+            #Checking for new events.
+            self.ship.update()
+            #The ship’s position will be updated after we’ve checked for keyboard
+            #events and before we update the screen. This allows the ship’s position to be
+            #updated in response to player input and ensures the updated position will
+            #be used when drawing the ship to the screen.
+            self._update_screen()
+            #Updating the screen on each pass through the loop.
 
-            #Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            #we fill the screen with the background color using the fill()method, 
-            #which acts on a surface and takes only one argument: a color.
-            #WE use self.settings to access the background color when filling the screen
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        # Watch for keyboard and mouse movements.
+        # This for loop is an event loop.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        #To access the events that Pygame detects, we’ll use the pygame.event.get() function.
+        #This function returns a list of events that have taken place
+        #since the last time this function was called. Any keyboard or mouse event
+        #will cause this for loop to run. Inside the loop, we’ll write a series of if
+        #statements to detect and respond to specific events. For example, when the
+        #player clicks the game window’s close button, a pygame.QUIT event is detected
+        #and we call sys.exit() to exit the game.
+            elif event.type == pygame.KEYDOWN:
+            #Inside _check_events() we add an elif block to the event loop to respond
+            #when Pygame detects a KEYDOWN event
+                if event.key == pygame.K_RIGHT:
+                #We check whether the key pressed,
+                #event.key, is the right arrow key
+                    #Move the ship to the right.
+                    self.ship.moving_right = True
+                    #we modify how the game responds when the player presses the
+                    #right arrow key: instead of changing the ship’s position directly, we merely
+                    #set moving_right to True.
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                    #If a KEYDOWN event occurs for the K_LEFT key, we set moving_left to True. If a
+                    #KEYUP event occurs for the K_LEFT key, we set moving_left to False. We can use
+                    #elif blocks here because each event is connected to only one key. If the player
+                    #presses both keys at once, two separate events will be detected.
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                    #we add a new elif block, which responds to
+                    #KEYUP events. When the player releases the right arrow key 
+                    #(K_RIGHT), we set moving_right to False.
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
-            #Make the most recently drawn screen visible.
-            pygame.display.flip()
 
-#The call to pygame.display.flip() tells Pygame to make the most
-#recently drawn screen visible. In this case, it simply draws an empty screen
-#on each pass through the while loop, erasing the old screen so only the new
-#screen is visible. When we move the game elements around, pygame.display.flip()
-#continually updates the display to show the new positions of game
-#elements and hides the old ones, creating the illusion of smooth movement.
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        #Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        #we fill the screen with the background color using the fill()method, 
+        #which acts on a surface and takes only one argument: a color.
+        #WE use self.settings to access the background color when filling the screen
+        self.ship.blitme()
+        #After filling the background, we draw the ship on the screen by calling
+        #ship.blitme, so the ship appears on top of the background.
+
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
+        #The call to pygame.display.flip() tells Pygame to make the most
+        #recently drawn screen visible. In this case, it simply draws an empty screen
+        #on each pass through the while loop, erasing the old screen so only the new
+        #screen is visible. When we move the game elements around, pygame.display.flip()
+        #continually updates the display to show the new positions of game
+        #elements and hides the old ones, creating the illusion of smooth movement.
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,20 +1,60 @@
+#We import the pygame module before defining the class.
 import pygame
+
 
 class Ship:
     """A class to manage the ship."""
 
+# The __init__() method of Ship takes two parameters: the self reference and a 
+# reference to the current instance of the AlienInvasion class.
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        #we assign the screen to an attribute of Ship, so we 
+        # can access it easily in all the methods in this class.
         self.screen = ai_game.screen
-        self.screen_react = ai_game.screen.get_rect()
 
-        #Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        #we access the screen’s rect attribute using the get_rect() method 
+        #and assign it to self.screen_rect. Doing so allows us to place the ship in the
+        #correct location on the screen.
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #Calling pygame and give it the location of our ship image.
+        #This function returns a surface representing the 
+        #ship, which we assign to self.image.
+        self.image = pygame.image.load('C:/Users/ajfm88/Sites/python_work/alien_invasion/images/ship.bmp')
+        #When the image is loaded, we call get_rect() to access the ship 
+        #surface’s rect attribute so we can later use it to place the ship.
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        #We’ll position the ship at the bottom center of the screen. To do so,
+        #make the value of self.rect.midbottom match the midbottom attribute of the
+        #screen’s rect
+
+        #Movement flag
+        self.moving_right = False
+        #We add a self.moving_right attribute in the __init__() method and set it
+        #to False initially
+        self.moving_left = False
+
+    #Then we add update(), which moves the ship right if the flag is True
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+            #The update() method will be called through an instance of
+            #Ship, so it’s not considered a helper method.
+        if self.moving_left:
+            self.rect.x -= 1
+            #In update(), we use two
+            #separate if blocks rather than an elif to allow the ship’s rect.x value to be
+            #increased and then decreased when both arrow keys are held down. This
+            #results in the ship standing still. If we used elif for motion to the left,
+            #right arrow key would always have priority. 
 
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+        #we define the blitme() method, which draws the image to the
+        # screen at the position specified by self.rect.
