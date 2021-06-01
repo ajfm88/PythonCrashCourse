b839791a683b3f83e0cfabd6c78849f1e3e11f31
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-05-31 20:42:50 -0700

    May 31, 2021

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -97,7 +97,7 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
-            sysexit()
+            sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
 
